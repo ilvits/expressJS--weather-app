ba19f308d1ec459db49523a7d1b5082d3493ac9a
ILViTs <60173598+ilvits@users.noreply.github.com>
2023-03-29 13:43:38 +0400

    - propertly configured service worker with workbox

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: 'public/',
+	globPatterns: [
+		'**/*.{png,css,ico,woff2,svg,html,js,webmanifest,md}'
+	],
+	swDest: 'public/sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
+};
